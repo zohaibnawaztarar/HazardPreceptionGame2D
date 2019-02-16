4150b80e4877b256f3e03e9306d47bb05d4a34ce
Zohaib Nawaz Tarar <zohaib_nawaz786@hotmail.com>
2019-02-16 23:45:01 +0000

    Added Track, Traffic and Player

---
+++ Assets/Scripts/enemyCarMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyCarMove : MonoBehaviour
+{
+    public float speed = 8.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(2, 0, 0) * speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/enemyDestroyer.cs
+++ Assets/Scripts/enemyDestroyer.cs
@@ -2,46 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class carController : MonoBehaviour
+public class enemyDestroyer : MonoBehaviour
 {
-    public float carSpeed;
-    
-    Vector3 pos;
-
-    public float maxPos = 2.1f;
-   
- 
-
     // Start is called before the first frame update
     void Start()
     {
-        pos = transform.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-    
         
-       pos.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
-
-       pos.x = Mathf.Clamp(pos.x, -2.1f, 2.1f);
-
-        transform.position = pos;
-
-      
-
-
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-
         if (col.gameObject.tag == "Enemy Car")
         {
-            Destroy(gameObject);
+            Destroy (col.gameObject);
 
         }
+
     }
 }
